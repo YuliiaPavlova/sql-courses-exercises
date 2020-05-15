aa1e8f1d1a4c652f15475f4f08c6125f0d3c68b7
YuliiaPavlova <yuliia.pavlovaa@gmail.com>
2020-05-15 12:00:38 +0200

    added solutions

--- stanford-course-SQLite/social_network_queries.sql
+++ stanford-course-SQLite/social_network_queries.sql
@@ -84,6 +84,14 @@ ORDER BY grade, name;
 
 -- 7. For each student A who likes a student B where the two are not friends, find if they have a friend C in common (who can introduce them!). For all such trios, return the name and grade of A, B, and C.
 
+SELECT DISTINCT H1.name, H1.grade, H2.name, H2.grade, H3.name, H3.grade
+FROM Highschooler H1, Highschooler H2, Highschooler H3, Likes L, Friend F1, Friend F2
+WHERE (H1.ID = L.ID1 AND H2.ID = L.ID2) AND H2.ID NOT IN (
+  SELECT ID2
+  FROM Friend
+  WHERE ID1 = H1.ID
+) AND (H1.ID = F1.ID1 AND H3.ID = F1.ID2) AND (H2.ID = F2.ID1 AND H3.ID = F2.ID2);
+
 -- 8. Find the difference between the number of students in the school and the number of different first names.
 
 SELECT COUNT(*) - COUNT(DISTINCT name) AS dif
@@ -107,6 +115,10 @@ HAVING COUNT(ID1)>1);
 
 -- 1. For every situation where student A likes student B, but student B likes a different student C, return the names and grades of A, B, and C.
 
+SELECT H1.name, H1.grade, H2.name, H2.grade, H3.name, H3.grade
+FROM Highschooler H1, Highschooler H2, Highschooler H3, Likes L1, Likes L2
+WHERE H1.ID = L1.ID1 AND H2.ID = L1.ID2 AND (H2.ID = L2.ID1 AND H3.ID = L2.ID2 AND H3.ID != H1.ID);
+
 -- 2. Find those students for whom all of their friends are in different grades from themselves. Return the students' names and grades.
 
 SELECT name, grade FROM Highschooler WHERE NOT EXISTS (
@@ -122,10 +134,16 @@ ORDER BY grade, name;
 SELECT AVG(friends) FROM 
 (SELECT COUNT(ID2) AS friends
 FROM Friend
-GROUP BY ID1);
+GROUP BY ID1); 
 
 -- 4. Find the number of students who are either friends with Cassandra or are friends of friends of Cassandra. Do not count Cassandra, even though technically she is a friend of a friend.
 
+SELECT COUNT(*)
+FROM Friend
+WHERE ID1 IN 
+  (SELECT ID2 FROM Friend WHERE ID1 IN 
+    (SELECT ID FROM Highschooler WHERE name = 'Cassandra'));
+    
 -- 5. Find the name and grade of the student(s) with the greatest number of friends.
 
 SELECT name, grade
